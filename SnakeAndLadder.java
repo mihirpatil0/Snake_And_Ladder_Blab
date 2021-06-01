7b13bbcfe98ede953bcd09b173599eea9cba1692
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-01 16:10:17 +0530

    Initialized startpoint as 0 and printing it

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,10 @@
 package snakeandladder;
 
 public class SnakeAndLadder 
-{
+{	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Snake and Ladder Game.");
+		int startPosition = 0;
+		System.out.println("Starting position is" + " " + startPosition );
 	}
 }
